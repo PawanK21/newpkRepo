0a0722dd370ee6ed56d540e959e0ce122b76f431
PawanK21 <thelimitlesspawankumar@gmail.com>
2021-10-02 18:49:09 +0530

    fetches_all_matches project updated

--- cheerio_Projects/cricinfo/fetchs_all_matches.js
+++ cheerio_Projects/cricinfo/fetchs_all_matches.js
@@ -1,8 +1,8 @@
 const request = require("request");
 const cheerio = require("cheerio");
 const xlsx = require("xlsx");
-const exceljs = require("exceljs");
-const writeXlsxFile = require("write-excel-file");
+// const exceljs = require("exceljs");
+// const writeXlsxFile = require("write-excel-file");
 
 let cricUrl =
   "https://www.espncricinfo.com/series/ipl-2020-21-1210595/match-results";
@@ -118,4 +118,100 @@ function subMatchCallback(matchId, err, res, html) {
     xlsx.writeFile(newWb, "cricInfo_IPL_seriesData.xlsx");
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+// const request = require("request");
+// const cheerio = require("cheerio");
+// const xlsx = require("xlsx");
+// request(
+//   "https://www.espncricinfo.com/series/ipl-2020-21-1210595/match-results",
+//   requestCallBack
+// );
+
+// function requestCallBack(err, res, html) {
+//   const $ = cheerio.load(html);
+//   const allMatchesAnchorTags = $('[data-hover = "Scorecard"]');
+
+//   // console.log(allMatchesAnchorTags.length);
+//   for (let i = 0; i < allMatchesAnchorTags.length; i++) {
+//     //allMatchesAnchorTags.length;
+//     let url =
+//       "https://www.espncricinfo.com/" + $(allMatchesAnchorTags[i]).attr("href");
+
+//     let arrayForID = url.split("-");
+//     let matchId = arrayForID[arrayForID.length - 2].substring(0, 7);
+//     // console.log(matchId);
+
+//     request(url, subMatchCallback.bind(this, matchId));
+//     // console.log(url);
+//   }
+// }
+
+// let newWb = xlsx.utils.book_new();
+// let count = 0;
+// function subMatchCallback(matchId, err, res, html) {
+//   const $ = cheerio.load(html);
+//   let tables = $(".Collapsible__contentInner tbody");
+//   // let batsmanProfileUrls = [];
+//   // let bowlerProfileUrls=[];
+//   // let tableobject = [];
+
+//   for (let i = 0; i < tables.length; i++) {
+
+//     count++;
+//     let titleOfSheet = `${matchId + "_Table -" + count}`;
+//     let table_rows = $(tables[i]).find("tr");
+
+//     // console.log(table.length);
+//     let tableobject = [];
+
+//     for (let j = 0; j < table_rows.length; j++) {
+//       let allCols = $(table_rows[j]).find("td");
+//       if (i % 2 == 0) {
+//         let hastheclass = $(allCols[0]).hasClass("batsman-cell");
+//         if (hastheclass == true) {
+//           tableobject.push({
+//             name: $(allCols[0]).text(),
+//             wickets_taken_by: $(allCols[1]).text(),
+//             runs: $(allCols[2]).text(),
+//             balls: $(allCols[3]).text(),
+//             fours: $(allCols[5]).text(),
+//             sixes: $(allCols[6]).text(),
+//             SR: $(allCols[7]).text(),
+//           });
+//         }
+//         // tableobject.push(batsmanProfileUrls);
+//       } else {
+//         let hastheclass = $(allCols[0]).hasClass("text-nowrap");
+//         if (hastheclass == true) {
+//           tableobject.push({
+//             name: $(allCols[0]).text(),
+//             overs: $(allCols[1]).text(),
+//             median_over: $(allCols[2]).text(),
+//             runs: $(allCols[3]).text(),
+//             wickets: $(allCols[5]).text(),
+//             econ: $(allCols[6]).text(),
+//             Fours: $(allCols[7]).text(),
+//             Sixes: $(allCols[8]).text(),
+//             WD: $(allCols[9]).text(),
+//           });
+//         }
+//         // tableobject.push(bowlerProfileUrls);
+//       }
+//     }
+
+//     let newWs = xlsx.utils.json_to_sheet(tableobject);
+//     xlsx.utils.book_append_sheet(newWb, newWs, titleOfSheet);
+//     xlsx.writeFile(newWb, "New_cricInfo_IPL_seriesData.xlsx");
+//   }
+// }
 
